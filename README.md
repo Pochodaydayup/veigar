c9d1b9b586d8636e42a62af620930db4c82c1da1
Pochodaydayup <13343460801@163.com>
2020-01-02 20:38:53 +0800

    optimize the development process

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Veigar
 
-使用完整的 vue 构建头条小程序。
+## 开发流程
+
+1. git clone git@github.com:Pochodaydayup/veigar.git
+2. yarn
+3. yarn dev:mp
+4. 用头条开发者工具打开 veigar/packages/veigar/examples/mp 即可调试
+
+## TODO
+
+- [ ] veigar-cli 的开发
+  - [ ] 接入 webpack
+  - [ ] 完成 veigar-loader
+    - [ ] compile .vue 文件
+  - [ ] 完成 veigar-webpack-plugin 插件
+    - [ ] typescript(tsx) 友好支持
+    - [ ] 将每个 page 分别打包，将每个 js 文件分别打包
+
+- [ ] veigar 的开发
+  - [ ] comment 的优化，目前是会创建一个空节点
+  - [ ] ...
+
+- [x] 开发流程的优化
--- packages/veigar/rollup.config.js
+++ packages/veigar/rollup.config.js
@@ -1,5 +1,6 @@
 import ts from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
+import alias from '@rollup/plugin-alias';
 
 export default {
   input: 'src/index.ts',
@@ -7,5 +8,13 @@ export default {
     file: './examples/mp/runtime/veigar.dev.js',
     format: 'esm',
   },
-  plugins: [resolve(), ts()],
+  plugins: [
+    alias({
+      entries: {
+        '@vue/runtime-core': '../../vue-next/packages/runtime-core/index.js',
+      },
+    }),
+    resolve(),
+    ts(),
+  ],
 };
